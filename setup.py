beb657cd7708c0ba5d786cc488ffbbb3b1104187
Chiara Paci <chiara.paci@gmail.com>
2015-01-19 10:18:09 +0100

    Added FileTag 2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='santaclara_pages',
-    version='0.2.2.1',
+    version='0.2.2.2',
     packages=['santaclara_pages'],
     package_data={'santaclara_pages': [
             "management/__init__.py",
