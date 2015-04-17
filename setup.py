3b9b40faae13b3a3bdb13d4c5db7209f7dda916f
Chiara Paci <chiara.paci@gmail.com>
2015-04-17 13:10:33 +0200

    Migrating to Django 1.9 2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='santaclara_pages',
-    version='0.3.1.1',
+    version='0.3.1.2',
     packages=['santaclara_pages'],
     package_data={'santaclara_pages': [
             "management/__init__.py",
